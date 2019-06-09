0e4d747a3a46950053189705f9c1b0bed021fca1
Derik Ramirez <rderik@gmail.com>
2019-06-08 19:10:04 -0600

    Added test to validate presence of name

--- test/models/term_test.rb
+++ test/models/term_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class TermTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "Invalid without name" do
+     term = Term.new(name: nil, content: nil)
+     refute term.valid? 'user is valid without a name'
+     assert_not_nil term.errors[:name], 'no validation error for name present'
+   end
 end

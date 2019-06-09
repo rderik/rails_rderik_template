3bd28b1998c84b4ff570804a76d4848a9ae1f631
Derik Ramirez <rderik@gmail.com>
2019-06-08 20:16:25 -0600

    Added a bug on test

--- test/models/term_test.rb
+++ test/models/term_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class TermTest < ActiveSupport::TestCase
    test "Invalid without name" do
-     term = Term.new(name: nil, content: nil)
+     term = Term.new(name: 'hello', content: nil)
      refute term.valid? 'user is valid without a name'
      assert_not_nil term.errors[:name], 'no validation error for name present'
    end
